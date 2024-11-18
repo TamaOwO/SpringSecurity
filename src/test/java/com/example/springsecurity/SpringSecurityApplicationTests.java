ae9382c6ca0b1b7f82972720397d5e4e9576f64b
Tama <mytamhuynhhmt@gmail.com>
2024-11-18 08:03:11 +0700

    Initial commit

---
+++ src/test/java/com/example/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
